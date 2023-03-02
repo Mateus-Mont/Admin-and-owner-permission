50e07fc95509f848818d2c782aa34fce62f146e4
Mateus-Mont <mateusmonteiropereira2@gmail.com>
2023-03-02 10:57:08 -0300

    refactor/ files

--- src/controllers/login.controllers.ts
+++ src/controllers/login.controllers.ts
@@ -1,11 +1,8 @@
 import {Request, Response } from "express";
-import { iLoginRequest } from "../interfaces/loginData";
 import {createLoginService}from "../services/login/createLogin.service"
 
 export const loginControllers=async(req:Request,res:Response):Promise<Response>=>{
 
   const token = await createLoginService(req.body)
-  return res.json({
-      token: token
-  })
+  return res.json({token: token})
 }
